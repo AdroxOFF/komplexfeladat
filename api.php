a0a7f8285b7f097b4abb33cc355eb7aa046638ca
AdroxOFF <theszabipresents2@gmail.com>
2026-04-18 17:18:58 +0200

    Add CORS headers to API response

--- api.php
+++ api.php
@@ -1,1 +1,6 @@
-
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type");
+?>
