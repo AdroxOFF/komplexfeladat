271ba10f7cacf5fb2cbf9d99637b9aa83f441ed5
AdroxOFF <theszabipresents2@gmail.com>
2026-04-18 17:19:47 +0200

    Add database connection setup in api.php

--- api.php
+++ api.php
@@ -3,4 +3,14 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type");
+
+// --- NETHELYES ADATOK --- ;)
+$servername = "localhost";
+$username = "szab1234"; 
+$password = "szab1234";
+$dbname = "szab1234";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
+
 ?>
